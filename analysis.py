9639bb61c56b6178e8a615708c9c8993ad73124a
Brendan Ryan <brendantipp@gmail.com>
2020-04-11 17:31:53 +0100

    updated comments and structure added imprt code

--- analysis.py
+++ analysis.py
@@ -1,2 +1,32 @@
 #Brendan Project Programming and Scripting
-#
+
+
+#Import libraries to be used for anaylsis
+#Brief description of what each library does?
+
+#import functions.py if needed ?
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+## Import the iris.csv dataset
+
+df = pd.read_csv("iris.csv")
+
+
+#Program outputs 
+
+
+
+#output summary of each variable to a single txt file 
+
+
+
+#create a histogram of each variable and save the graph/plot as a png file 
+
+
+
+#output a scatter plot of each pair of variables 
