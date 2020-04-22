eb5b85648d4d9a833ab9ec17a12c6f6c2fbe555b
Brendan Ryan <brendantipp@gmail.com>
2020-04-22 15:17:19 +0100

    added seaborn library for plots

--- analysis.py
+++ analysis.py
@@ -9,6 +9,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+import seaborn as sns
 
 
 ## Import the iris.csv dataset
@@ -42,17 +44,33 @@ plt.savefig("sepal_length_hist.png")
 #plt.clf()
 
 #3 output a scatter plot of each pair of variables
+#https://matplotlib.org/3.1.0/gallery/subplots_axes_and_figures/subplots_demo.html
 
-plt.scatter (df['sepal_length'], df['sepal_width'])
-plt.title ("Sepal Lenght versus Sepal Width")
-plt.xlabel("Sepal Length")
-plt.ylabel("Sepal Length")
-plt.ylabel("Sepal Width")
-#plt.show()
+a = df["sepal_length"]
+b = df["sepal_width"]
+c = df["petal_length"]
+d = df["petal_width"]
 
-plt.scatter (df['petal_length'], df['petal_width'])
-plt.title ("Petal Lenght versus Petal Width")
-plt.xlabel("Petal Length")
-plt.ylabel("Petal Length")
-plt.ylabel("Petal Width")
-#plt.show()
+fig, ((ax1,ax2),(ax3,ax4)) = plt.subplots(nrows=2, ncols=2,figsize=(8, 4))
+
+ax1.scatter(x=a, y=b, marker='o', c='r', edgecolor='b')
+ax1.set_title('Scatter: $a$ versus $b$')
+ax1.set_xlabel('$a$')
+ax1.set_ylabel('$b$')
+
+ax2.scatter(x=c, y=d, marker='o', c='r', edgecolor='b')
+ax2.set_title('Scatter: $c$ versus $d$')
+ax2.set_xlabel('$c$')
+ax2.set_ylabel('$d$')
+
+ax3.scatter(x=c, y=d, marker='o', c='r', edgecolor='b')
+ax3.set_title('Scatter: $c$ versus $a$')
+ax3.set_xlabel('$c$')
+ax3.set_ylabel('$d$')
+
+ax4.scatter(x=c, y=d, marker='o', c='r', edgecolor='b')
+ax4.set_title('Scatter: $c$ versus $a$')
+ax4.set_xlabel('$c$')
+ax4.set_ylabel('$d$')
+
+plt.show()
