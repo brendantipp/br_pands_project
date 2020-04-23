f8e11cf582c7aaeb3a1a9949690e54a58793f5ec
Brendan Ryan <brendantipp@gmail.com>
2020-04-23 15:41:18 +0100

    updated summary code and comments incl readme

--- analysis.py
+++ analysis.py
@@ -4,33 +4,35 @@
 #Import libraries to be used for anaylsis
 #Brief description of what each library does?
 
-#import functions.py if needed ?
 
-import pandas as pd
+import pandas as pd #abbreviate library to simplyfy code
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import seaborn as sns
 
-## Import the iris.csv dataset
 
+# Import the iris.csv dataset and name as df
 df = pd.read_csv("iris.csv")
+#name the colums 
+names=['sepal_length','sepal_width','petal_length','petal_width','type']
+
 
 #Program outputs 
 #1 output summary of each variable to a single txt file using pandas
-#https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_csv.html
 #https://www.datacourses.com/write-a-pandas-dataframe-to-a-csv-file-218/
 #https://backtobazics.com/python/pandas-describe-method-dataframe-summary/
+#https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_csv.html
+#Create a summary using pandas .describe function
 
+summary = df.describe().round(2).transpose() ##For better presentation using the .round() function to round to 2 decimal places
+summary.to_csv('summary.txt',mode = "w",sep="\t") #output to csv using the .to_csv function, mode w to create new file each time \t to serpate outputu using tabs
 
-#use the pandas .describe() function to create a summary table and sing the round() fucntion to round the % to 2 decimal places for better presentation
-summary = df.describe().round(2)
-#using the numpy transpose function returns the axis of the table for better presentation
-#summary = summary.transpose() 
-#print(summary)
-#print ("Please note this summary has also been outputed to a text file summary.txt")
-#using the \t tab seperator for better presenation of the summary table
-#summary.to_csv('summary.txt',sep="\t")
+#decided to include a summary counts of the type of species not include in .describe function
+summary2 = df["species"].value_counts()
+summary2.to_csv('summary.txt',mode = "a",sep="\t", header="false") # using mode append to add to single text file
+
+print ("Please note this summary has also been outputed to a text file summary.txt") # to display on screen
+
 
 
 #2 create a histogram of each variable and save the graph/plot as a png file 
@@ -41,13 +43,13 @@ summary = df.describe().round(2)
 
 plt.hist(df["sepal_length"])
 plt.title("Sepal Length")
+plt.xlabel("Sepal_Length_cm") 
+plt.ylabel("Count") 
 #plt.show()
 #plt.savefig("sepal_length_hist.png")
-sns.FacetGrid(df,hue="species").map(sns.distplot,"sepal_length").add_legend()
+sns.FacetGrid(df,hue="species",height =4).map(sns.distplot,"sepal_length").add_legend()
 plt.show()
-sns.FacetGrid(df,hue="species").map(sns.distplot,"sepal_length").add_legend()
-plt.show()
-#plt.savefig("sepal_length_hist_species.png")
+pl#plt.savefig("sepal_length_hist_species.png")
 #clear the axis
 plt.clf()
 
