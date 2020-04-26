7dba81a1e7295b1168a3b6b8902ba16c8f3372ba
Brendan Ryan <brendantipp@gmail.com>
2020-04-26 15:06:03 +0100

    updated scatter plot to add plot title

--- analysis.py
+++ analysis.py
@@ -1,20 +1,19 @@
-#Brendan Project Programming and Scripting
-
+#Brendan Project Programming and Scripting 2020
+#GMIT 
+#Anaylis of the Fishers Iris Data Set
 
 #Import libraries to be used for anaylsis
-#Brief description of what each library does?
-
-
+#Description of each library can be found in readme file
 import pandas as pd #abbreviate library to simplyfy code
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
+import matplotlib.pyplot as plt #abbreviate library to simplyfy code
+import seaborn as sns #abbreviate library to simplyfy code
+import numpy as np #abbreviate library to simplyfy code
 
 
 # Import the iris.csv dataset and name as df
 df = pd.read_csv("iris.csv")
-#after research define specias as type category thanks to https://github.com/simonava5/fishers-iris-data
-df['species'] = df['species'].astype('category')
+#after research define species as type category thanks to https://github.com/simonava5/fishers-iris-data
+df['species'] = df['species'].astype('category') #category to be used for summary file
 
 
 #Program outputs 
@@ -25,46 +24,46 @@ df['species'] = df['species'].astype('category')
 
 
 #Create a summary using pandas .describe function
-summary = df.describe().round(2).transpose() ##For better presentation using the .round() function to round to 2 decimal places
-summary.to_csv('presentation\summary.csv',mode = "w",sep="\t", header = "false") #output to csv text using the .to_csv function, mode w to create new file each time \t to serpate outputu using tabs
+##For better presentation using the .round() function to round to 2 decimal places
+summary = df.describe().round(2).transpose() 
+#output to txt using the .to_csv function,
+#decieded on txt over csv file type for on screen presentation
+#used mode w to create new file each time \t to seperate output using tabs
+summary.to_csv('presentation\summary.txt',mode = "w",sep="\t") 
 
-#wanted to include a summary counts of each category of species not included in .describe function by using panas groupby function
+#wanted to include a summary counts of each category of species not included in .describe function 
+#by using panas groupby function
 by_species = df.groupby('species')
-summary2 = by_species.describe().transpose().round(2) #use transpose to invert axis for better presentation
-summary2.to_csv('presentation\summary.csv',mode = "a",sep="\t", header="false") # using mode append to add to single csv text file
-
-
-# print ("Please note this summary has also been outputed to a text file summary.txt") # to display on screen
-
+#use transpose to invert axis for better presentation
+summary2 = by_species.describe().transpose().round(2) 
+#using mode append to add to single text file
+summary2.to_csv('presentation\summary.txt',mode = "a",sep="\t") 
 
 #2 create a histogram of each variable and save the graph/plot as a png file 
 #https://www.datacamp.com/community/tutorials/histograms-matplotlib
 #################################
+#look at placing the following histogram script in a for loop?
 
-plt.hist(df["sepal_length"])
-plt.title("Sepal Length Histogram")
-plt.xlabel("Sepal_Length_cm") 
-plt.ylabel("Count /Numbers") 
-plt.savefig("presentation\sepal_length_hist.png")
-plt.clf()
-
+plt.hist(df["sepal_length"]) #plot histogram using matplotlib hist function
+plt.title("Analysis of Sepal Length Histogram") #insert a title for graph
+plt.xlabel("Sepal_Length_cm") #title the x axis
+plt.ylabel("Count /Numbers") #title the y axis
+plt.savefig("presentation\sepal_length_hist.png") #out to png file and save location
+plt.clf() #clear the axises
 
 plt.hist(df["sepal_width"])
-plt.title("Sepal width Histogram")
+plt.title("Analysis of Sepal width Histogram")
 plt.xlabel("Sepal_width_cm") 
 plt.ylabel("Count /Numbers") 
 plt.savefig("presentation\sepal_width_hist.png")
 plt.clf()
 
-
 plt.hist(df["petal_length"])
-plt.title("Petal Length Histogram")
+plt.title("Analyis of Petal Length Histogram")
 plt.xlabel("Petal_Length_cm") 
 plt.ylabel("Count /Numbers") 
 plt.savefig("presentation\petal_length_hist.png")
 plt.clf()
-
-
 
 plt.hist(df["petal_width"])
 plt.title("Petal Width Histogram")
@@ -72,34 +71,49 @@ plt.xlabel("Petal_Width_cm")
 plt.ylabel("Count /Numbers") 
 plt.savefig("presentation\petal_width_hist.png")
 plt.clf()
-
 plt.close()
 
-#diplay all for comparison simply using a matplotlib histogram
+#diplay all varibales for comparison simply using a matplotlib histogram
+#https://stackoverflow.com/questions/19614400/add-title-to-collection-of-pandas-hist-plots
 df.hist()
-plt.show()
+plt.suptitle("Analysis of all Four Variables")
+plt.savefig("presentation\histogram_analysis.png")
+plt.clf()
+plt.close()
+#plt.show() #display the histogram if required
 
 
+
+#used a seaborn facetgrid and displot to further analysis any link between 
+#the speal and petal lenght versus the category of species
 #https://medium.com/@avulurivenkatasaireddy/exploratory-data-analysis-of-iris-data-set-using-python-823e54110d2d
 #https://seaborn.pydata.org/generated/seaborn.FacetGrid.html
+#https://stackoverflow.com/questions/26163702/how-to-change-figuresize-using-seaborn-factorplot
 
-
-sns.FacetGrid(df,hue="species",height=3).map(sns.distplot,"sepal_length").add_legend()
-sns.FacetGrid(df,hue="species",height=3).map(sns.distplot,"petal_length").add_legend()
-
-plt.show()
+sns.FacetGrid(df,hue="species",height=6,aspect=2).map(sns.distplot,"sepal_length",).add_legend()
+plt.ylabel ("count / numbers")
+plt.savefig("presentation\sepal_length_by_category.png")
+sns.FacetGrid(df,hue="species",height=6, aspect=2).map(sns.distplot,"petal_length").add_legend()
+plt.ylabel ("count / numbers")
+plt.savefig("presentation\petal_length_by_category.png")
+#plt.show()
 
 ################################
 
 #3 output a scatter plot of each pair of variables
-#https://matplotlib.org/3.1.0/gallery/subplots_axes_and_figures/subplots_demo.html
+#https://stackoverflow.com/questions/26139423/plot-different-color-for-different-categorical-levels-using-matplotlib
 #https://elitedatascience.com/python-seaborn-tutorial
 #https://seaborn.pydata.org/tutorial/axis_grids.html
-#also tested pairplot
-#tps://medium.com/codebagng/basic-analysis-of-the-iris-data-set-using-python-2995618a6342
+#https://medium.com/codebagng/basic-analysis-of-the-iris-data-set-using-python-2995618a6342
+#https://stackoverflow.com/questions/46307941/how-can-i-add-title-on-seaborn-lmplot
 
 
-sns.set_style("whitegrid");
-sns.pairplot(df, hue="species", height=2);
-plt.savefig("presentation\scatter_plot.png")
-plt.show()
+#sets the style for the seaborn pairplot
+#the datarame , hue split dataframe up into colours based on species category
+p = sns.pairplot(df, hue="species",height=2)
+fig = p.fig
+fig.suptitle("Scatterplot analysis of Irish Dat Set Variables")
+plt.savefig("presentation\scatter_plot.png") #desitination of plot created
+#plt.show() #display on screen if required 
+
+print("Thanks, all presentation items are now created in the Presenation folder - I hope you enjoy and have a nice day!")
